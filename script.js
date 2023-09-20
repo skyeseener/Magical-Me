111cbcf60c51d99aee7fe93a1dcc629d8c4d5e56
skyeseener <143412223+skyeseener@users.noreply.github.com>
2023-09-20 17:54:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function greatestAchievement() {
+   var password = document.getElementById("password2");
+   var passwordText = password.value;
+   if(passwordText == "Playing guitar with Maneskin") {
+    return true;
+   }
+  alert("TRY AGAIN!!!");
+   return false;
+   }
